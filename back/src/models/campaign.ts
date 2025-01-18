9ebf199f67fed508e22ca0a4bf45c3d3b0ffbf79
chanchanhi <eugenie.nyp@gmail.com>
2025-01-18 17:31:46 +0900

    Fix: src/models/campaigns.ts API 설계대로 수정

--- back/src/models/campaign.ts
+++ back/src/models/campaign.ts
@@ -1,19 +1,27 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface ICampaign extends Document {
-  title: string;
+  campaignID: number;
+  campaignTitle: string;
   goalAmount: number;
   currentAmount: number;
   deadline: Date;
+  progress: number;
+  imageUrl: string;
   description: string;
+  createdAt: Date; // 신규 펀딩 정렬 기준
 }
 
 const CampaignSchema: Schema = new Schema({
-  title: { type: String, required: true },
+  campaignID: { type: Number, required: true, unique: true },
+  campaignTitle: { type: String, required: true },
   goalAmount: { type: Number, required: true },
   currentAmount: { type: Number, default: 0 },
   deadline: { type: Date, required: true },
+  progress: { type: Number, default: 0 },
+  imageUrl: { type: String, required: true },
   description: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ICampaign>('Campaign', CampaignSchema);
